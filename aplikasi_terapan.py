572175d6ef719b492ca9e9ea529a81550d478052
sunnybudi <sunnybuditama@gmail.com>
2025-06-18 19:05:17 +0700

    Update aplikasi_terapan.py

--- aplikasi_terapan.py
+++ aplikasi_terapan.py
@@ -141,7 +141,7 @@ with tab1:
     keuntungan = [total_laba_meja, total_laba_kursi, total_keuntungan_bersih]
     
     x_pos = np.arange(len(kategori))
-    width = 0.80  # lebar batang
+    width = 0.65  # lebar batang
     
     fig2, ax2 = plt.subplots(figsize=(24, 18))
     
