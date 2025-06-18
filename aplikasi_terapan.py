a6729a1f07412378a561a05467fcf4bfaba13b38
sunnybudi <sunnybuditama@gmail.com>
2025-06-18 12:32:17 +0700

    Update aplikasi_terapan.py

--- aplikasi_terapan.py
+++ aplikasi_terapan.py
@@ -150,12 +150,12 @@ with tab1:
     ax2.plot(produk_x, penjualan_x, 'x--', color='darkblue', alpha=0.7, label='Penjualan Meja (X)')
     ax2.plot(produk_y, penjualan_y, 'x--', color='darkgreen', alpha=0.7, label='Penjualan Kursi (Y)')
 
-    def label_titik(produk, nilai, warna):
+    def label_titik(produk, nilai, warna, offset_y=50_000, offset_x=0):
         indeks = [0, len(produk)//2, len(produk)-1]
         for i in indeks:
             if nilai[i] != 0:
-                 ax2.text(produk[i] + offset_x, nilai[i] + offset_y, format_rupiah(nilai[i]),
-                     fontsize=8, color=warna, ha='left', va='bottom')
+                ax2.text(produk[i] + offset_x, nilai[i] + offset_y, format_rupiah(nilai[i]),
+                         fontsize=8, color=warna, ha='left', va='bottom')
 
     label_titik(produk_x, keuntungan_x, 'black')                      # Meja
     label_titik(produk_y, keuntungan_y, 'black', offset_x=0.5)        # Kursi
