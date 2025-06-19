e7622edabbc0e1c24ad5785b9ca1480730a246ee
sunnybudi <sunnybuditama@gmail.com>
2025-06-19 11:28:27 +0700

    Update aplikasi_terapan.py

--- aplikasi_terapan.py
+++ aplikasi_terapan.py
@@ -149,7 +149,7 @@ with tab1:
     x_pos = np.arange(len(kategori))
     width = 0.35  # lebar batang
     
-    fig2, ax2 = plt.subplots(figsize=(16, 10))
+    fig2, ax2 = plt.subplots(figsize=(20, 10))
     
     # Batang vertikal
     bar1 = ax2.bar(x_pos - width/2, keuntungan, width=width, color='skyblue', label='Keuntungan')
